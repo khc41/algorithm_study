beecd00a7ac3d8879b1c3745a5fa8774072865a7
heechul <khc4190@naver.com>
2022-02-24 23:29:50 +0900

    2022.02.24

---
+++ backjoon/exhaustive/6603.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool visit[14];
+int num[14];
+int k;
+int ans[6];
+
+void dfs(int n, int depth) {
+	ans[depth] = num[n];
+	if (depth == 6) {
+		for (int i = 0; i < 6; i++)
+			cout << ans[i] << " ";
+		cout << "\n";
+		return;
+	}
+	visit[n] = true;
+	for (int i = n + 1; i < k; i++) {
+		if (!visit[i]){
+			dfs(i, depth + 1);
+		}
+	}
+	visit[n] = false;
+}
+
+int main() {
+	cin >> k;
+	while (k != 0) {
+		memset(num, 0, sizeof(num));
+		memset(visit, false, sizeof(visit));
+		for (int i = 0; i < k; i++) {
+			cin >> num[i];
+		}
+		for (int i = 0; i < k - 5; i++){
+			dfs(i, 0);
+			cout << "\n";
+		}
+		cin >> k;
+	}
+}
