541bc97c832926148dd1499a1966fe27143daec6
heechul <khc4190@naver.com>
2024-12-20 13:56:36 +0900

    2024.12.20

---
+++ programmers/더맵게.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int sco : scoville){
+            pq.add(sco);
+        }
+
+        while(!pq.isEmpty()){
+            if(pq.size() > 1){
+                int first = pq.poll();
+                int second = pq.poll();
+                if(first >= K){
+                    return answer;
+                }
+                pq.add(first + second * 2);
+                answer++;
+            } else {
+                if(pq.poll() >= K){
+                    return answer;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
