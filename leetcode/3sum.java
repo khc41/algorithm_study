c7b83e618d1d470db4df0e595f663d2628deabff
heechul <khc4190@naver.com>
2025-07-30 22:08:02 +0900

    add threeSum function to find all unique triplets in an array that sum to zero

---
+++ leetcode/3sum.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Set<List<Integer>> result = new HashSet<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum < 0) {
+                    left++;
+                } else if (sum > 0) {
+                    right--;
+                } else {
+                    List<Integer> list = List.of(nums[i], nums[left], nums[right]);
+                    result.add(list);
+                    left++;
+                    right--;
+                }
+            }
+        }
+        return new ArrayList<>(result);
+    }
+}
