d9820bc56db497a3365bf80142922f5f83eb6b62
khc41 <khc4190@naver.com>
2025-08-29 15:54:15 +0900

    refactor dailyTemperatures to simplify logic and improve readability

--- leetcode/daily-temperatures.java
+++ leetcode/daily-temperatures.java
@@ -2,25 +2,13 @@ class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
         Stack<Integer> stack = new Stack<>();
         int[] result = new int[temperatures.length];
-        stack.add(0);
-        int prev = temperatures[0];
-        for (int i = 1; i < temperatures.length; i++) {
-            if (prev > temperatures[i]) {
-                stack.add(i);
-                prev = temperatures[i];
-                continue;
-            }
-            while (!stack.isEmpty()) {
+
+        for (int i = 0; i < temperatures.length; i++) {
+            while (!stack.isEmpty() && temperatures[stack.peek()] < temperatures[i]) {
                 int index = stack.pop();
-                if (temperatures[index] < temperatures[i]) {
-                    result[index] = i - index;
-                } else {
-                    stack.add(index);
-                    break;
-                }
+                result[index] = i - index;
             }
             stack.add(i);
-            prev = temperatures[i];
         }
         return result;
     }
