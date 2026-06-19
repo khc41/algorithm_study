c57d20b31658a2a002a25ee50e480b1cbcabb34c
khc41 <khc4190@naver.com>
2026-06-19 23:44:40 +0900

    refactor mergeAlternately function to simplify merging logic and handle remaining characters

--- leetcode/merge-strings-alternately.kt
+++ leetcode/merge-strings-alternately.kt
@@ -1,22 +1,21 @@
 class Solution {
     fun mergeAlternately(word1: String, word2: String): String {
-        val size1 = word1.length
-        val size2 = word2.length
-
-        if(size1 > size2) {
-            return merge(word1, word2, word1.substring(size2), size2)
-        } else{
-            return merge(word1, word2, word2.substring(size1), size1)
-        }
-    }
-
-    fun merge(word1: String, word2: String, lastString: String, size: Int) : String {
         val sb = StringBuilder()
-        for(i in 0 until size) {
-            sb.append(word1[i])
-            sb.append(word2[i])
+        var i = 0
+        val len1 = word1.length
+        val len2 = word2.length
+
+        // 두 문자열 중 하나라도 문자가 남아있다면 계속 반복
+        while (i < len1 || i < len2) {
+            if (i < len1) {
+                sb.append(word1[i])
+            }
+            if (i < len2) {
+                sb.append(word2[i])
+            }
+            i++
         }
-        sb.append(lastString)
+
         return sb.toString()
     }
 }
