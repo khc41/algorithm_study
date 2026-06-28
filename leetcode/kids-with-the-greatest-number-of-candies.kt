7b5ed817aa69eca200e56517724d461874dccef0
khc41 <khc4190@naver.com>
2026-06-28 13:10:33 +0900

    refactor kidsWithCandies to use map for concise implementation

--- leetcode/kids-with-the-greatest-number-of-candies.kt
+++ leetcode/kids-with-the-greatest-number-of-candies.kt
@@ -1,10 +1,6 @@
 class Solution {
     fun kidsWithCandies(candies: IntArray, extraCandies: Int): List<Boolean> {
         val maxCandy = candies.max()
-        val result = mutableListOf<Boolean>()
-        for (i in candies.indices) {
-            result.add(candies[i] + extraCandies >= maxCandy)
-        }
-        return result
+        return candies.map { it + extraCandies >= maxCandy}
     }
 }
