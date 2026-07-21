186d6bdd420ed96d63c88bf826a62a3b9e997c33
khc41 <khc4190@naver.com>
2026-07-21 20:59:37 +0900

    refactor string-compression solution for improved readability and efficiency

--- leetcode/string-compression.kt
+++ leetcode/string-compression.kt
@@ -2,31 +2,25 @@ class Solution {
     fun compress(chars: CharArray): Int {
         var rp = 0
         var wp = 0
-        var lastChar = chars[0]
-        var repeat = 0
+
         while (rp < chars.size) {
-            if (lastChar != chars[rp] && wp <= rp) {
-                chars[wp++] = lastChar
-                lastChar = chars[rp]
-                if (repeat != 1) {
-                    for (c in repeat.toString()) {
-                        chars[wp++] = c
-                    }
-                }
-                repeat = 0
-                continue
-            } else if (rp == chars.size - 1) {
-                chars[wp++] = lastChar
+            val currentChar = chars[rp]
+            var repeat = 0
+
+            while (rp < chars.size && chars[rp] == currentChar) {
+                rp++
                 repeat++
-                if (repeat != 1) {
-                    for (c in repeat.toString().toCharArray()) {
-                        chars[wp++] = c
-                    }
+            }
+
+            chars[wp++] = currentChar
+
+            if (repeat != 1) {
+                for (c in repeat.toString()) {
+                    chars[wp++] = c
                 }
             }
-            rp++
-            repeat++
         }
+
         return wp
     }
 }
