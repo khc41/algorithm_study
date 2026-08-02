18e779cd9d8396795bbbe73b989d79a3f8f66d16
khc41 <khc4190@naver.com>
2026-08-02 19:38:38 +0900

    refactor: optimize is-subsequence solution using hashmap and binary search

--- leetcode/is-subsequence.kt
+++ leetcode/is-subsequence.kt
@@ -1,18 +1,34 @@
 class Solution {
     fun isSubsequence(s: String, t: String): Boolean {
-        var cur = 0
-        val chars = s.toCharArray()
-        if (s.length == 0) {
-            return true
+        var map = hashMapOf<Char, ArrayList<Int>>()
+        for (i in t.indices) {
+            map.getOrPut(t[i]) { arrayListOf() }.add(i)
         }
-        for (i in t) {
-            if (chars[cur] == i) {
-                if (cur == s.length - 1) {
-                    return true;
-                }
-                cur++
+        var last = -1
+        for (i in s) {
+            val list = map[i] ?: return false
+            val idx = upperBound(list, last)
+
+            if (idx >= list.size) return false
+            last = list[idx]
+        }
+        return true
+    }
+
+    fun upperBound(list: ArrayList<Int>, target: Int): Int {
+        var left = 0
+        var right = list.size
+
+        while (left < right) {
+            val mid = left + (right - left) / 2
+
+            if (list[mid] <= target) {
+                left = mid + 1
+            } else {
+                right = mid
             }
         }
-        return false
+
+        return left
     }
 }
