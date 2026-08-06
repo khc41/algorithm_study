751e5e72779a5469cfc03e08341615e7137cab25
khc41 <khc4190@naver.com>
2026-08-06 22:24:31 +0900

    refactor: simplify findMaxAverage implementation for better readability and performance

--- leetcode/maximum-average-subarray-i.kt
+++ leetcode/maximum-average-subarray-i.kt
@@ -1,18 +1,15 @@
 class Solution {
     fun findMaxAverage(nums: IntArray, k: Int): Double {
         var sum = 0.0
-        var result = 0.0
-        for (i in 0 until nums.size) {
-            if (i < k) {
-                sum += nums[i]
-                if (i == k - 1) {
-                    result = sum / k
-                }
-                continue
-            }
-            sum += (nums[i] - nums[i - k])
-            result = maxOf(result, sum / k)
+        for (i in 0 until k) {
+            sum += nums[i]
         }
-        return result
+        var maxSum = sum
+
+        for (i in k until nums.size) {
+            sum += (nums[i] - nums[i - k])
+            maxSum = maxOf(sum, maxSum)
+        }
+        return maxSum / k
     }
 }
