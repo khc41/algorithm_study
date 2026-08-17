3463d5510a17e871966549c99d7129739a5f8462
khc41 <khc4190@naver.com>
2026-08-17 16:49:52 +0900

    refactor: optimize maxConsecutiveOnesIII for improved performance and clarity

--- leetcode/max-consecutive-ones-iii.kt
+++ leetcode/max-consecutive-ones-iii.kt
@@ -4,17 +4,19 @@ class Solution {
         var rp = 0
         var result = 0
         var zeros = 0
-        while (lp < nums.size && rp < nums.size) {
+
+        while (rp < nums.size) {
             if (nums[rp] == 0) zeros++
-            result = maxOf(result, rp - lp)
+
             while (zeros > k) {
                 if (nums[lp] == 0) {
                     zeros--
                 }
                 lp++
             }
+            result = maxOf(result, rp - lp + 1)
             rp++
         }
-        return maxOf(result, rp - lp)
+        return result
     }
 }
