63ae9ea59fd81523700dc0528f90c867fe6ced62
khc41 <khc4190@naver.com>
2026-08-18 21:17:04 +0900

    refactor: optimize longestSubarray implementation for clarity and efficiency

--- leetcode/longest-subarray-of-1s-after-deleting-one-element.kt
+++ leetcode/longest-subarray-of-1s-after-deleting-one-element.kt
@@ -1,11 +1,10 @@
 class Solution {
     fun longestSubarray(nums: IntArray): Int {
         var left = 0
-        var right = 0
         var k = 0
         var result = 0
 
-        while (right < nums.size) {
+        for (right in nums.indices) {
             if (nums[right] == 0) k++
 
             while (k > 1) {
@@ -13,7 +12,6 @@ class Solution {
             }
 
             result = maxOf(result, right - left)
-            right++
         }
 
         return result
