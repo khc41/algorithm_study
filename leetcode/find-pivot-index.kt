dba2bcf851bd406fdb922d232ac6735e906293cd
khc41 <khc4190@naver.com>
2026-08-24 23:11:52 +0900

    refactor: simplify pivotIndex implementation for improved readability and performance

--- leetcode/find-pivot-index.kt
+++ leetcode/find-pivot-index.kt
@@ -1,20 +1,18 @@
 class Solution {
     fun pivotIndex(nums: IntArray): Int {
-        val array = IntArray(nums.size)
-        array[nums.size - 1] = 0
-        var sum = 0
-        for (i in (nums.size - 2) downTo 0) {
-            sum += nums[i + 1]
-            array[i] = sum
-        }
-        if (array[0] == 0) return 0
-        sum = 0
-        for (i in 1 until nums.size) {
-            sum += nums[i - 1]
-            if (sum == array[i]) {
+        val total = nums.sum()
+        var left = 0
+
+        for (i in nums.indices) {
+            val right = total - left - nums[i]
+
+            if (left == right) {
                 return i
             }
+
+            left += nums[i]
         }
+
         return -1
     }
 }
